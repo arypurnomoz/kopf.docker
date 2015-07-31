25986948dba346aa3b19187b6fe28f40fd02e7ae
Ary Purnomoz <arypurnomoz@gmail.com>
2015-07-31 20:07:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+echo '{"elasticsearch_root_path": "/es"}' > /kopf/kopf_external_settings.json
+
 if [ "$KOPF_CONFIG" ]; then 
   echo "$KOPF_CONFIG" > /kopf/kopf_external_settings.json
 fi
@@ -8,5 +12,10 @@ if [ -f "/config/kopf_external_settings.json" ]; then
   mv -f /config/kopf_external_settings.json /kopf/kopf_external_settings.json
 fi
 
+envtpl /etc/nginx/nginx.conf.tpl
 
-exec /run.sh
+if [ ! -z "${KOPF_BASIC_AUTH_LOGIN}" ]; then
+    echo "${KOPF_BASIC_AUTH_LOGIN}:${KOPF_BASIC_AUTH_PASSWORD}" > /etc/nginx/kopf.htpasswd
+fi
+
+exec nginx
